d3a39016630dc878474b3b605657181314a42ac9
eita115115 <eita115115@users.noreply.github.com>
2026-06-13 01:12:14 +0900

    Add axis dispersion analysis, sight mark interpolation, end/session editing, update banner, score distribution, monthly summary, triple-spot face, dark mode

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "archery-note-v2";
+const CACHE = "archery-note-v3";
 const ASSETS = ["./index.html", "./manifest.json", "./icon.svg", "./apple-touch-icon.png"];
 
 self.addEventListener("install", e => {
