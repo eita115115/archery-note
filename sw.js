a361d2912af22cfbd29049b43e1edae9f523b3ba
eita115115 <eita115115@users.noreply.github.com>
2026-06-14 13:23:18 +0900

    Publish v6 line contact scoring

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "archery-note-v3";
+const CACHE = "archery-note-v6";
 const ASSETS = ["./index.html", "./manifest.json", "./icon.svg", "./apple-touch-icon.png"];
 
 self.addEventListener("install", e => {
