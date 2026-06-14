aa32fbcbaf00e84848f3a1d01a1501f6c1052127
eita115115 <eita115115@users.noreply.github.com>
2026-06-14 19:19:52 +0900

    Publish v12 precision physics and gear workflow

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "archery-note-v11";
+const CACHE = "archery-note-v12";
 const ASSETS = ["./index.html", "./manifest.json", "./icon.svg", "./apple-touch-icon.png"];
 
 self.addEventListener("install", e => {
