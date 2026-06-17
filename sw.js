7945e3f0f848d25653b75bfe1c85ea2fea1b59ce
eita115115 <eita115115@users.noreply.github.com>
2026-06-17 18:41:42 +0900

    Release v34 converge-inspired record flow

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "archery-note-v33";
+const CACHE = "archery-note-v34";
 const ASSETS = ["./index.html", "./manifest.json", "./icon.svg", "./apple-touch-icon.png"];
 
 self.addEventListener("install", e => {
